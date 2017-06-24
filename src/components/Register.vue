38fb86c1ddd30296084c42c9d37a3f22331d6e36
2XI <shengxi_hu@yeah.net>
2017-06-24 13:06:58 +0800

    写注册页，协作同步

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,9 +1,25 @@
 <template>
 	<div class="register">
 		<img class="app_logo" src="../assets/app_logo.png">
-		<Input  placeholder="请输入...">
-		<span slot="prepend"></span>
+		<Input class="input_box"  placeholder="请输入...">
+		<span slot="prepend"><Icon type="person"></Icon></span>
 		</Input>
+		<Input  placeholder="请输入...">
+		<span slot="prepend"><Icon type="ios-telephone"></Icon></span>
+		</Input>
+		<Input  placeholder="请输入...">
+		<span slot="prepend"><Icon type="locked"></Icon></Icon></span>
+		</Input>
+		<Input  placeholder="请输入...">
+		<span slot="prepend"><Icon type="locked"></Icon></Icon></span>
+		</Input>
+		<Select v-model="model2" size="small" style="width:100px">
+			<Option v-for="item in cityList" :value="item.value" :key="item">{{ item.label }}</Option>
+		</Select>
+		<Select v-model="model2" size="small" style="width:100px">
+			<Option v-for="item in cityList" :value="item.value" :key="item">{{ item.label }}</Option>
+		</Select>
+		<Button type="success" long>确认提交</Button>
 	</div>
 </template>
 
