47c5747d47ed7dd877cee0edd1fcd75666fa25a9
2XI <shengxi_hu@yeah.net>
2017-06-24 09:56:43 +0800

    添加了一些必要的配置，将iview的css拿出来放在static里面

--- src/main.js
+++ src/main.js
@@ -5,12 +5,13 @@ import App from './App'
 import router from './router'
 import store from './store'
 import VueResource from 'vue-resource'
-import iView from 'iview';
-import '../node_modules/.npminstall/iview/2.0.0-rc.17/iview/dist/styles/iview.css';
+import iView from 'iview'
+import '../static/styles/iview.css'
 
 
 Vue.config.productionTip = false
 Vue.use(VueResource)
+Vue.use(iView)
 
 /* eslint-disable no-new */
 new Vue({
