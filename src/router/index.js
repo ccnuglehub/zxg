be17d98746e08fefd4895502983430b63687fade
gtanghaibo <tab231@163.com>
2017-06-26 04:30:18 +0800

    在router中添加了myproject

--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,9 @@ import Register from '@/components/Register'
 import Work from '@/components/Work'
 import AddProject from '@/components/AddProject'
 import WorkerDetail from '@/components/WorkerDetail'
-<<<<<<< HEAD
 import Newslist from '@/components/Newslist'
 import NewsDetail from "@/components/NewsDetail"
-=======
->>>>>>> a8358c1aa55c4d3257ff357d1ff895353e640bb1
+import Myproject from "@/components/Myproject"
 
 Vue.use(Router)
 
@@ -49,6 +47,11 @@ export default new Router({
       path: '/worker_detail',
       name: 'worker_detail',
       component: WorkerDetail
+    },
+    {
+      path: '/myproject',
+      name: 'myproject',
+      component: Myproject
     },
     {
       path: '*',
