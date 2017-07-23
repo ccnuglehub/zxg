67026e0976783a2e2b8de6591d90cba6e0568497
2XI <shengxi_hu@yeah.net>
2017-07-23 21:27:13 +0800

    合并接口

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,24 +1,43 @@
 <template>
-<div style="height:100%">
-<div style="height:50%"></div>
-  <Input v-model="value" placeholder="请输入电话号码" type="text" style="width: 60%;margin:auto;"></Input>
-    <Button type="info" v-tap="{ methods: login }">登录</Button>
-</div>
-    
+    <div style="height:100%">
+        <div style="height:40%"></div>
+        <Input v-model="user_tel" @on-blur="phoneValidate(user_tel)" placeholder="请输入电话号码" type="text" style="width: 60%;margin:auto;"></Input>
+        <Button class="login_bt" type="success" v-tap="{ methods: login }">登录</Button>
+        <div class="input error"><span v-show="!phone_flag">姓名格式不正确</span></div>
+    </div>
 </template>
 <script>
 import {API_ROUTER_CONFIG} from '@/api/config/api_config'
-    export default {
-        data () {
-            return {
-                value: ''
-            }
-        },
-        methods:{
-          login(){
-            // alert(API_ROUTER_CONFIG.login)
-
-          }
+import { checkPhone, checkEmpty } from '../util/util.js'
+export default {
+    data () {
+        return {
+            user_tel: '',
+            phone_flag:true
+        }
+    },
+    methods:{
+        checkPhone,
+        checkEmpty,
+        phoneValidate(val){
+			this.phone_flag = this.checkPhone(val)
+		},
+        login(){
+            if(!this.phone_flag || this.checkEmpty(this.user_tel)) {
+				return
+			}
+            alert(API_ROUTER_CONFIG.login)
         }
     }
+}
 </script>
+<style scoped>
+.error {
+	text-align: left;
+	padding-left: 40px;
+    padding-top: 4px;
+	color: red;
+	height: 20px;
+}
+</style>
+
